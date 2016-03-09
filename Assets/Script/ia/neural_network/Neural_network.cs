7f199a99f325d19a617b7bbeaaf1a89597a4791e
fuggy <anton.francois134@gmail.com>
2016-03-09 18:02:49 +0100

    quelques bout de début

---
+++ Assets/Script/ia/neural_network/Neural_network.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Nn {
+
+    public class Neural_network {
+        List<List<Input>> network;
+
+        // ====== constructors ==================
+
+        // default contructor, build a perceptron
+        public Neural_network() {
+            List<Neuron> layer = new List<Neuron>;
+
+
+        }
+
+        public class Neural_network(List<int> n_neuronIlayer){
+
+        return 0;
+    }
+}
+	
+        
+        
+    
+     //======= Useless =======================   
+    /*        
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    */
+}
