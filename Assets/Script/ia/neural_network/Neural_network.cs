9361877911c334b6b2442d986dfaadc8679a060e
fuggy <anton.francois134@gmail.com>
2016-03-19 23:34:56 +0100

    in process edition - NON WORKING VERSION
    Surtout pour que Quentin puisse voir les méthodes existantes et
    s'adaptent a elle.

--- Assets/Script/ia/neural_network/Neural_network.cs
+++ Assets/Script/ia/neural_network/Neural_network.cs
@@ -5,36 +5,76 @@ using System.Collections.Generic;
 namespace Nn {
 
     public class Neural_network {
-        List<List<Input>> network;
+        public List<List<Neuron>> network;
+        List<bool> output;
 
-        // ====== constructors ==================
+        // ====== CONSTRUCTORS ==================
 
-        // default contructor, build a perceptron
-        public Neural_network() {
-            List<Neuron> layer = new List<Neuron>;
+        // Make a neural network with all input at zero and random weights
+        // minimal constructor
+        public Neural_network(int n_Layer, List<int> n_neuronIlayer)
+        {
+            Neuron tmp;
+            List<Neuron> tmp_layer;
+            network = new List<List<Neuron>>();
+            for (int i = 0; i < n_Layer; i++)
+            {
+                tmp_layer = new List<Neuron>();
+                for (int j = 0; j < n_neuronIlayer[i]; j++)
+                {
+                    if (i == 1)  // special treatment for first layer, that take input of the network
+                    {                                                // for this case the weight is    
+                        List<double> n = new List<double>(); n.Add(1); // a List of one element valued at 1 
+                        tmp = new Neuron(n);
+                    }
+                    else
+                    {
+                        tmp = new Neuron(n_neuronIlayer[i - 1]); // fill Input with n_neuronIlayer[i - 1] random values
+                    }
+                    tmp_layer.Add(tmp);
+                }
+                this.network.Add(tmp_layer);
+            }
+        }
+        
+        // If specific weight are needed use this constructor
+        public Neural_network(List<List<List<double>>> network_weight)
+        {
+            Neuron tmp;
+            List<Neuron> tmp_layer;
+            List<double> tmp_weight;
+            network = new List<List<Neuron>>();
+            for (int i = 0; i < network_weight.Count; i++)
+            {
+                tmp_layer = new List<Neuron>();
+                for (int j = 0; j < network_weight[i].Count; j++)
+                {
+                    tmp_weight = new List<double>(network_weight[i][j]);
+                    tmp = new Neuron(tmp_weight);
+                    tmp_layer.Add(tmp);
+                }
+                this.network.Add(tmp_layer);
+            }
+        }
 
+
+        // ===== METHODS =========================
+
+            // = GET/SET =========================
+
+       
+        
+
+            // = USEFULL METHODS =================
+        public List<bool> fire(List<double> input)
+        {
+            // test
+                // si input.Count == network[1].Count
+                // tout les input 0<...<1
+            // fait le transfert layer par layer des input
 
         }
 
-        public class Neural_network(List<int> n_neuronIlayer){
 
-        return 0;
     }
-}
-	
-        
-        
-    
-     //======= Useless =======================   
-    /*        
-    // Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    */
 }
--- Assets/Script/ia/neural_network/Neuron.cs
+++ Assets/Script/ia/neural_network/Neuron.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,62 +7,127 @@ namespace Nn
 {
     public struct Input
     {
-        public bool input { get; set; }
+        public double input { get; set; }
         public double weight { get; set; }
     }
 
     public class Neuron  // enlever le MonoBehaviour
     {
         public List<Input> inputs;
-        public double thershold { get; set; }
-        public bool fire { get; private set; }  // is the neuron activated ?  default: FALSE 
+        public double fire_val { get; private set; }  // is the neuron activated ?  default: FALSE
+        //public int in_layer { get; set; }
+        //public int in_position { } 
 
-        private Random rand = new Random();
+        private System.Random rand;
         
         // ======== CONSTRUCTOR ==============================
 
         // this Neuron is initialited with random weights
+        // and Input.input values at 0
         public Neuron(int n_weights)
         {
+            // Input fill
             Input tmp;
             this.inputs = new List<Input>();
-
+            rand = new System.Random();
             for (int i = 0; i < n_weights; i++)
             {
                 tmp = new Input();
-                double p = rand.N
+                tmp.weight = rand.NextDouble(); tmp.input = 0;
+                inputs.Add(tmp);
             }
-
         }
 
         public Neuron(List<double> weight){
-            this.inputs = new List<Input>;
-            thershold = 0.5;
+            // Input fill
+            Input tmp;
+            this.inputs = new List<Input>();
+            for (int i = 0; i < weight.Count; i++)
+            {
+                tmp = new Input();
+                tmp.weight = weight[i]; tmp.input = 0;
+                inputs.Add(tmp);
+            }
+        }
+        
+        public Neuron(List<double> weight , List<double> input)
+        {
+            if(weight.Count != input.Count)
+            {
+                Debug.Log("The weight and the input list have different size ! What did you expect ?");
+                throw new IndexOutOfRangeException();
+            }
+            // Input fill
+            Input tmp;
+            this.inputs = new List<Input>();
+            for (int i = 0; i < weight.Count; i++)
+            {
+                tmp = new Input();
+                tmp.weight = weight[i]; tmp.input = input[i];
+                inputs.Add(tmp);
+            }
+            // Take the descision
+            main(inputs);
         }
 
-
+        // The most secure way to construct the Neuron class with the
+        // Input list made. 
+        public Neuron(List<Input> inputs)
+        {
+            // Input fill
+            this.inputs = new List<Input>(inputs);
+            // Take the descision
+            main(inputs);
+        }
         //========== Methods =======================================
 
-
-        public void setWeights(double weight, int position) {
-            if (inputs.Count <= position)
+            // ===== GET/SET =======================================
+        public void setWeight(double weights, int position) {
+            if (inputs.Count <= position)      
+                inputs[position].weight = weights;
+            else
             {
-                this.inputs[position].weight= weight;
+                Debug.Log("try to set weight in a too high position");
+                throw new IndexOutOfRangeException();
             }
-            else  Debug.Log("try to set weight in a too high position");
+        }
 
+        public void setInput(double input, int position)
+        {
+            if (inputs.Count <= position)
+                inputs[position].input = input;
+            else
+            {
+                Debug.Log("try to set input in a too high position");
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void setInput(Input input, int position)
+        {
+            if( inputs. Count <= position)
+                inputs[position] = input;
+            else
+            {
+                Debug.Log("try to set Input in a too high position");
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+           // ===== USEFUL Methods =================================
+
+        private void main(List<Input> inputs)
+        {
+            double val = combinaison(inputs);
+            activation(val);
         }
 
         // Function that sum up the weigthed values returned by the input neurons 
         public double combinaison(List<Input> inputs) {
             double val = 0;
-            int tmp_input;
 
             for (int i = 0; i < inputs.Count; i++){
-                if (inputs[i].input) tmp_input = 1; // transform the bool value in integer
-                else tmp_input = 0;
-
-                val = val + inputs[i].weight * tmp_input;
+                val = val + inputs[i].weight * inputs[i].input;
             }
 
             return val;
@@ -71,17 +137,12 @@ namespace Nn
         // Function that descide weather the neuron fire
         // val    : must be in ]0,1]
         // lambda : in orther to control the slope of the function, the more lambda is high, the more the slope is important
-        public bool activation(double val, double lambda =1.0){
-            bool descision;
+        public double activation(double val, double lambda =1.0){
 
+            // ACTIVATION FUNCTION
             val = 1 / (1 + Mathf.Exp( (float) (-lambda * val)));  // sigmoid function
-            if (val < 0.5){
-                descision = false;
-            }
-            else{
-                descision = true;
-            }
-            return descision;
+            this.fire_val = val;
+            return val;
         }
     }
 }
